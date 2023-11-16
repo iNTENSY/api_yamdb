e991c7e1868e7fda2c668506ed163dfcf45d5784
KolumbChris <chriskolumb@rambler.ru>
2023-11-16 10:57:32 +0300

    Reviews/comments
    Добавил ревью и комменты.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -34,3 +34,24 @@ class Title(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.author.username} on {self.content.title}'
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on Review {self.review.id}'
